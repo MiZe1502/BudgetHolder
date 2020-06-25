810666f3d6dd39fc4add5647d15e81443730d58e
MiZe1502 <z.com@mail.ru>
2020-06-25 22:46:39 +0300

    added google maps builder template

---
+++ src/common/components/MapContainer/GoogleMapsBuilder.ts
@@ -0,0 +1,52 @@
+import { PlacemarkBuilder } from "./PlacemarkBuilder";
+import { MapsBuilder } from "./MapsBuilder";
+
+
+declare var google;
+
+export class GoogleMapsBuilder implements MapsBuilder {
+    container: HTMLDivElement = null;
+    map: any = null;
+    placementBuilder?: PlacemarkBuilder;
+
+    defaultZoom: number = 8;
+
+    constructor(container: any, placementBuilder?: PlacemarkBuilder) {
+        this.container = container;
+        this.placementBuilder = placementBuilder;
+    }
+
+    private createMap(coords: Coordinates) {
+		this.map = new google.maps.Map(this.container, {
+            zoomGmaps: this.defaultZoom,
+			centerGmaps: coords,
+		});
+    }
+
+    findSingleAddressAndCreateMap(address: string): void {
+
+		// const map = new google.maps.Map(container, {
+        //     zoomGmaps,
+		// 	centerGmaps,
+		// });
+
+
+        // const locations = [{
+        //     latitude: 55.75361503443606,
+        //     longitude: 37.620883000000006,
+        //     name: 'Test point'
+        // }];
+
+        //const centerGmaps = {lat: -34.397, lng: 150.644};
+
+        // const points = myMap.geoObjects;
+        // locations.forEach(location => {
+        //     points.add(new ymaps.Placemark(
+        //         [location.latitude, location.longitude],
+        //             {balloonContent: location.name},
+        //             {
+        //     }));
+        // });
+
+    }
+}
--- src/common/components/MapContainer/MapContainer.svelte
+++ src/common/components/MapContainer/MapContainer.svelte
@@ -10,42 +10,17 @@
     import { onMount } from 'svelte';
 
     onMount(async () => {
-		// const map = new google.maps.Map(container, {
-        //     zoomGmaps,
-		// 	centerGmaps,
-		// });
-
-
-
-        // const points = myMap.geoObjects;
-        // locations.forEach(location => {
-        //     points.add(new ymaps.Placemark(
-        //         [location.latitude, location.longitude],
-        //             {balloonContent: location.name},
-        //             {
-        //     }));
-        // });
-
         const config = {
             name,
             address,
         }
         const placemarkBuilder: PlacemarkBuilder = new YandexMapsPlacemarkBuilder(config);
-        const mapBuilder: MapBuilder = new YandexMapsBuilder(container, placemarkBuilder);
+        const mapBuilder: MapsBuilder = new YandexMapsBuilder(container, placemarkBuilder);
 
         mapBuilder.findSingleAddressAndCreateMap(config.address);
     })
 
     let container;
-
-    export let locations = [{
-        latitude: 55.75361503443606,
-        longitude: 37.620883000000006,
-        name: 'Test point'
-    }];
-    export let centerGmaps = {lat: -34.397, lng: 150.644};
-    export let centerYmaps = [55.75361503443606, 37.620883000000006];
-    export let zoomGmaps = 8;
 
     export let address: string = "";
     export let name: string = "";
