1fe21304d18012177f411329cbff9e66ed300f1e
MiZe1502 <z.com@mail.ru>
2020-09-04 20:23:05 +0300

    started db sql scripts

---
+++ db/db.sql
@@ -0,0 +1,10 @@
+-- DROP DATABASE budget_holder;
+
+CREATE DATABASE budget_holder
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'Russian_Russia.1251'
+    LC_CTYPE = 'Russian_Russia.1251'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
