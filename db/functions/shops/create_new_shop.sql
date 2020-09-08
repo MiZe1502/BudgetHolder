59446c64d61186caa7c9069adda851ea6bc48443
MiZe1502 <z.com@mail.ru>
2020-09-08 17:23:46 +0300

    started stored functions and procedures for shops

---
+++ db/functions/shops/create_new_shop.sql
@@ -0,0 +1,25 @@
+CREATE OR REPLACE FUNCTION create_new_shop(name VARCHAR(3000),
+                                           url VARCHAR(3000),
+                                           address VARCHAR(3000),
+                                           comment VARCHAR(3000),
+                                           user_session_id INTEGER)
+    RETURNS INTEGER AS
+$BODY$
+DECLARE user_by_session_id INTEGER;
+    DECLARE new_id INTEGER;
+BEGIN
+    SELECT user_id INTO user_by_session_id
+    FROM budget.sessions
+    WHERE id = user_session_id AND is_active=TRUE;
+
+    IF user_by_session_id IS NULL THEN
+        RAISE EXCEPTION 'No users found for session: %.', user_session_id;
+    END IF;
+
+    INSERT INTO budget.shops(name, url, address, comment, added_by_user_id)
+    VALUES (name, url, address, comment, user_by_session_id) RETURNING id INTO new_id;
+
+    RETURN new_id;
+END
+$BODY$
+    LANGUAGE plpgsql;
