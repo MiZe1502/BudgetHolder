f0e32cdc9cd6d5e227128a69a56ca44d478d4f64
MiZe1502 <z.com@mail.ru>
2020-09-21 14:52:49 +0300

    implemented sql function to get goods categories chain

---
+++ db/functions/goods_categories/get_goods_categories_chain_by_id.sql
@@ -0,0 +1,39 @@
+SET search_path TO budget;
+
+CREATE OR REPLACE FUNCTION get_goods_categories_chain_by_id(top_category_id INTEGER)
+    RETURNS TABLE(id INTEGER,
+                  name VARCHAR(150),
+                  parent_id INTEGER,
+                  comment VARCHAR(3000)) AS
+$$
+    WITH RECURSIVE
+        starting (id, name, parent_id, comment) AS
+            (
+                SELECT gc.id, gc.name, gc.parent_id, gc.comment
+                FROM goods_categories AS gc
+                WHERE gc.id = top_category_id AND NOT gc.is_removed
+            ),
+        descendants (id, name, parent_id, comment) AS
+            (
+                SELECT s.id, s.name, s.parent_id, s.comment
+                FROM starting AS s
+                UNION ALL
+                SELECT gc.id, gc.name, gc.parent_id, gc.comment
+                FROM goods_categories AS gc JOIN descendants AS d ON gc.parent_id = d.id
+                WHERE NOT gc.is_removed
+            ),
+        ancestors (id, name, parent_id, comment) AS
+            (
+                SELECT gc.id, gc.name, gc.parent_id, gc.comment
+                FROM goods_categories AS gc
+                WHERE gc.id IN (SELECT parent_id FROM starting) AND NOT gc.is_removed
+                UNION ALL
+                SELECT gc.id, gc.name, gc.parent_id, gc.comment
+                FROM goods_categories AS gc JOIN ancestors AS a ON gc.id = a.parent_id
+                WHERE NOT gc.is_removed
+            )
+        TABLE ancestors
+    UNION ALL
+    TABLE descendants;
+$$
+    LANGUAGE sql;
