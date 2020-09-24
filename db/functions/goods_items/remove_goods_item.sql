3ff11bc3780430e60fc97fc98a51c82af40a01ab
MiZe1502 <z.com@mail.ru>
2020-09-24 18:28:18 +0300

    implemented sql function to remove goods item

---
+++ db/functions/goods_items/remove_goods_item.sql
@@ -0,0 +1,25 @@
+
+SET search_path TO budget;
+
+CREATE OR REPLACE FUNCTION remove_goods_item(goods_item_id INTEGER,
+                                             user_session_uuid UUID)
+    RETURNS INTEGER AS
+$BODY$
+    DECLARE user_by_session_id INTEGER;
+    DECLARE removed_id INTEGER;
+BEGIN
+    SELECT * INTO user_by_session_id
+    FROM get_user_id_by_session_uuid(user_session_uuid);
+
+    UPDATE goods
+    SET is_removed = TRUE,
+        updated_at = now(),
+        updated_by_user_id = user_by_session_id
+    WHERE id = goods_item_id
+    RETURNING id INTO removed_id;
+
+    RETURN removed_id;
+END
+$BODY$
+    LANGUAGE plpgsql;
+
