4cbd7fe5ad4ac9ec9f9e3383262651907218b066
MiZe1502 <z.com@mail.ru>
2020-09-28 15:14:26 +0300

    implemented sql function to get purchase by id

---
+++ db/functions/purchases/get_purchase_by_id.sql
@@ -0,0 +1,16 @@
+
+SET search_path TO budget;
+
+CREATE OR REPLACE FUNCTION get_purchase_by_id(purchase_id INTEGER)
+    RETURNS TABLE(id INTEGER,
+                  total_price MONEY,
+                  shop_id INTEGER,
+                  date DATE,
+                  comment VARCHAR(3000)) AS
+$$
+    SELECT DISTINCT id, total_price, shop_id, date, comment
+    FROM purchases
+    WHERE is_removed = FALSE
+      AND id = purchase_id
+$$
+    LANGUAGE sql;
