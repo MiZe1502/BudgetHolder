8341ff43fa2ae991702884d626f2d46a84335eb3
MiZe1502 <z.com@mail.ru>
2020-10-27 15:32:12 +0300

    added helper to make password hash and salt

---
+++ backend/be/utils/password.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func convertPasswordFromStringToByteArray(password string) []byte {
+	return []byte(password)
+}
+
+func convertPasswordFromByteArrayToString(password []byte) string {
+	return string(password)
+}
+
+// HashPasswordWithSalt prepares password to store it in db
+func HashPasswordWithSalt(password string) (string, error) {
+	pwd := convertPasswordFromStringToByteArray(password)
+    hash, err := bcrypt.GenerateFromPassword(pwd, bcrypt.MaxCost)
+    return convertPasswordFromByteArrayToString(hash), err
+}
+
+// ComparePasswords compares password from user and from db
+func ComparePasswords(hashedPwd string, plainPwd string) bool {
+	byteHash := convertPasswordFromStringToByteArray(hashedPwd)   
+	bytePwd := convertPasswordFromStringToByteArray(plainPwd)   
+	err := bcrypt.CompareHashAndPassword(byteHash, bytePwd)
+    if err != nil {
+        return false
+    }
+    
+	return true
+}
