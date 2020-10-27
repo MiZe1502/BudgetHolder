b1f89af4382d584e7db8606e6504637bbdf1dfc5
MiZe1502 <z.com@mail.ru>
2020-10-27 20:59:52 +0300

    started user login processing

--- backend/be/repositories/user.go
+++ backend/be/repositories/user.go
@@ -2,6 +2,9 @@ package repos
 
 import (
 	"context"
+	"errors"
+
+	utils "../utils"
 
 	"github.com/georgysavva/scany/pgxscan"
 )
@@ -20,7 +23,7 @@ type UserRepository struct {
 }
 
 // GetUserByLogin returns single user with its encoded password
-func (r *UserRepository) GetUserByLogin(login string) (User, error) {
+func (r *UserRepository) getUserByLogin(login string) (User, error) {
 	var user User
 
 	err := pgxscan.Get(context.Background(), r.db, &user, `SELECT * from budget.get_user_by_login($1)`, login)
@@ -29,4 +32,48 @@ func (r *UserRepository) GetUserByLogin(login string) (User, error) {
 	}
 
 	return user, err
+}
+
+func (r *UserRepository) createUserSession(login, ip string) (string, error) {
+	uuid := utils.GetNewUUID()
+
+	err := pgxscan.Get(context.Background(), r.db, &uuid, `SELECT * from budget.create_session($1, $2, $3)`, login, ip, uuid)
+	if err != nil {
+		return "", err
+	}
+
+	return uuid, nil
+}
+
+// ProcessUser contains processing login logic:
+// get user data from db by login
+// check password and its hash
+// create session and save it to db
+// create websocket connection and add it to Hub
+// create jwt token and return it as response
+func (r *UserRepository) ProcessUser(login string, password string, ip string) (string, error) {
+	user, err := r.getUserByLogin(login)
+	if err != nil {
+		r.log.Error(err.Error())
+		return "", err
+	}
+
+	isPasswordCorrect := utils.ComparePasswords(user.Password, password)
+	if !isPasswordCorrect {
+		err := errors.New("Incorrect password: " + password + " for user: " + login)
+		r.log.Error(err.Error())
+		return "", err
+	}
+
+	uuid, err := r.createUserSession(login, ip)
+	if err != nil {
+		r.log.Error(err.Error())
+		return "", err
+	}
+
+	r.log.Info("created session: " + uuid + " for user: " + login)
+
+
+
+	return "", nil
 }
