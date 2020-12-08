1ad90356102e26584f42464d3078c4dd01a18328
MiZe1502 <z.com@mail.ru>
2020-12-08 23:32:29 +0300

    implemented several goods items repository methods

--- backend/be/repositories/goods.go
+++ backend/be/repositories/goods.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/georgysavva/scany/pgxscan"
+	"github.com/google/uuid"
 )
 
 //GoodsItem represents goods data item
@@ -54,7 +55,7 @@ func (r *GoodsRepository) GetSlice(from int, count int) ([]*GoodsItem, error) {
 }
 
 //GetTopGoodsItemsByName finds top goods items by name
-func (r *ShopsRepository) GetTopGoodsItemsByName(name string) ([]*GoodsItem, error) {
+func (r *GoodsRepository) GetTopGoodsItemsByName(name string) ([]*GoodsItem, error) {
 	var goodsItems []*GoodsItem
 
 	err := pgxscan.Select(context.Background(), r.db, &goodsItems, `SELECT * from budget.get_top_goods_items_by_name($1, $2)`, name, TopCounter)
@@ -63,4 +64,57 @@ func (r *ShopsRepository) GetTopGoodsItemsByName(name string) ([]*GoodsItem, err
 	}
 
 	return goodsItems, err
+}
+
+//CreateNewGoodsItem creates new goods item and returns its id
+func (r *GoodsRepository) CreateNewGoodsItem(goodsItemData *GoodsItem, sessionUUID uuid.UUID) (int, error) {
+	var addedGoodsItemID int
+
+	err := pgxscan.Get(context.Background(),
+		r.db,
+		&addedGoodsItemID,
+		`SELECT * from budget.create_new_goods_item($1, $2, $3, $4, $5::uuid)`,
+		goodsItemData.Name,
+		goodsItemData.CategoryID,
+		goodsItemData.Comment,
+		goodsItemData.BarCode,
+		sessionUUID.String())
+	if err != nil {
+		return IncorrectID, err
+	}
+
+	return addedGoodsItemID, err
+}
+
+//RemoveEntityByID marks single goods item as removed in db
+func (r *GoodsRepository) RemoveEntityByID(id int, uuid uuid.UUID) (int, error) {
+	var removedGoodsItemID int
+
+	err := pgxscan.Get(context.Background(), r.db, &removedGoodsItemID, `SELECT * from budget.remove_goods_item($1, $2::uuid)`, id, uuid)
+	if err != nil {
+		return IncorrectID, err
+	}
+
+	return removedGoodsItemID, err
+}
+
+//UpdateGoodsItem updates existing goods item and returns its id
+func (r *GoodsRepository) UpdateGoodsItem(goodsItemData *GoodsItem, sessionUUID uuid.UUID) (int, error) {
+	var updatedGoodsItemID int
+
+	err := pgxscan.Get(context.Background(),
+		r.db,
+		&updatedGoodsItemID,
+		`SELECT * from budget.update_single_goods_item($1, $2, $3, $4, $5, $6::uuid)`,
+		goodsItemData.ID,
+		goodsItemData.Name,
+		goodsItemData.CategoryID,
+		goodsItemData.Comment,
+		goodsItemData.BarCode,
+		sessionUUID.String())
+	if err != nil {
+		return IncorrectID, err
+	}
+
+	return updatedGoodsItemID, err
 }
