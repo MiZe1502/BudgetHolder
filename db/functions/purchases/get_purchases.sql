2a055aa2aef5b91cf61f756a796ce7e065d4a828
MiZe1502 <z.com@mail.ru>
2020-12-11 23:16:14 +0300

    modified sql function to get purchases

--- db/functions/purchases/get_purchases.sql
+++ db/functions/purchases/get_purchases.sql
@@ -1,7 +1,8 @@
 SET search_path TO budget;
 
 CREATE OR REPLACE FUNCTION get_purchases(start INTEGER,
-                                         count INTEGER)
+                                         count INTEGER,
+                                         user_id INTEGER)
     RETURNS TABLE(id INTEGER,
                   total_price MONEY,
                   shop_id INTEGER,
@@ -11,6 +12,7 @@ $$
     SELECT id, total_price, shop_id, date, comment
     FROM purchases
     WHERE is_removed = FALSE
+      AND added_by_user_id = user_id
     ORDER BY created_at DESC
     LIMIT count
     OFFSET start;
