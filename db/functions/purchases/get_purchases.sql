860b2cc1b66c17cac846dc2cdf432cc5103aa28b
MiZe1502 <z.com@mail.ru>
2020-12-12 22:12:23 +0300

    changed sql function to get purchases and goods

--- db/functions/purchases/get_purchases.sql
+++ db/functions/purchases/get_purchases.sql
@@ -9,12 +9,12 @@ CREATE OR REPLACE FUNCTION get_purchases(start INTEGER,
                   date TIMESTAMP,
                   comment VARCHAR(3000)) AS
 $$
-SELECT id, total_price::money::numeric::float, shop_id, date::timestamp, comment
-FROM purchases
-WHERE is_removed = FALSE
-  AND added_by_user_id = user_id
-ORDER BY created_at DESC
-LIMIT count
-OFFSET start;
+    SELECT id, total_price::money::numeric::float, shop_id, date::timestamp, comment
+    FROM purchases
+    WHERE is_removed = FALSE
+      AND added_by_user_id = user_id
+    ORDER BY created_at DESC
+    LIMIT count
+    OFFSET start;
 $$
     LANGUAGE sql;
