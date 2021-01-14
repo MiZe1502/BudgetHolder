98177ad309261372e93558b64f409c101d001259
MiZe1502 <z.com@mail.ru>
2021-01-14 20:42:06 +0300

    added sql function to count all shops

---
+++ db/functions/shops/count_shops.sql
@@ -0,0 +1,14 @@
+SET search_path TO budget;
+
+CREATE OR REPLACE FUNCTION count_shops()
+RETURNS INTEGER AS
+$BODY$
+    DECLARE total INTEGER;
+BEGIN
+    SELECT COUNT(*) INTO total
+    FROM shops;
+
+    RETURN total;
+END
+$BODY$
+    LANGUAGE plpgsql;
