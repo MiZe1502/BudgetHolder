a528750d855580c0e45b54cff6f4993ad1a13592
MiZe1502 <z.com@mail.ru>
2021-02-04 23:31:05 +0300

    changed sql function to get list of simple goods items for dropdowns with suggestions

--- db/functions/goods_items/get_top_goods_items_by_name.sql
+++ db/functions/goods_items/get_top_goods_items_by_name.sql
@@ -1,14 +1,12 @@
 SET search_path TO budget;
 
-CREATE OR REPLACE FUNCTION get_top_goods_items_by_name(goods_item_name VARCHAR,
-                                                       top INTEGER)
+CREATE OR REPLACE FUNCTION get_simple_goods_items()
     RETURNS TABLE(id INTEGER,
                   name VARCHAR) AS
 $$
     SELECT id, name
     FROM goods
-    WHERE is_removed = FALSE AND name ~ goods_item_name
+    WHERE is_removed = FALSE
     ORDER BY name
-    LIMIT top;
 $$
     LANGUAGE sql;
