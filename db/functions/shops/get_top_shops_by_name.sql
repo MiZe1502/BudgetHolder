98f6ca85e5022f7b2f1e6205f42718712f7fc06d
MiZe1502 <z.com@mail.ru>
2021-02-05 23:42:46 +0300

    changed sql function to get list of simple shops

--- db/functions/shops/get_top_shops_by_name.sql
+++ db/functions/shops/get_top_shops_by_name.sql
@@ -1,14 +1,12 @@
 SET search_path TO budget;
 
-CREATE OR REPLACE FUNCTION get_top_shops_by_name(shop_name VARCHAR,
-                                                 top INTEGER)
+CREATE OR REPLACE FUNCTION get_simple_shops()
     RETURNS TABLE(id INTEGER,
                   name VARCHAR) AS
 $$
     SELECT id, name
     FROM shops
-    WHERE is_removed = FALSE AND name ~ shop_name
+    WHERE is_removed = FALSE
     ORDER BY name
-    LIMIT top;
 $$
-LANGUAGE sql;
+    LANGUAGE sql;
